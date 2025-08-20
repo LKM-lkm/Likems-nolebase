9aface0cd66988379c5ed9dabcbe688a6a1e5a6f
LKM-lkm <lkm836972@outlook.com>
2025-08-20 22:19:05 +0800

    修改mathjax

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -148,11 +148,7 @@ export default defineConfig({
       name: 'msapplication-TileColor',
       content: '#603cba',
     }],
-    // TeX Gyre Termes 字体用于数学公式
-    ['link', { 
-      rel: 'stylesheet', 
-      href: 'https://fonts.cdnfonts.com/css/tex-gyre-termes' 
-    }],
+
     // Proxying Plausible through Netlify | Plausible docs
     // https://plausible.io/docs/proxy/guides/netlify
     ['script', { 'defer': 'true', 'data-domain': 'nolebase.ayaka.io', 'data-api': '/api/v1/page-external-data/submit', 'src': '/assets/page-external-data/js/script.js' }],
@@ -254,7 +250,11 @@ export default defineConfig({
     math: true,
     config: (md) => {
       md.use(MarkdownItFootnote)
-      md.use(MarkdownItMathjax3)
+      md.use(MarkdownItMathjax3, {
+        output: {
+          font: 'mathjax-termes'
+        }
+      })
       md.use(BiDirectionalLinks({
         dir: process.cwd(),
       }))
