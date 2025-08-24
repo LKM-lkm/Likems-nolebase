8dfabc7a685026c6b18de6aa686fca0c566c81fe
LKM-lkm <lkm836972@outlook.com>
2025-08-24 13:31:39 +0800

    fix: 按照源码修复

--- .vitepress/theme/components/HomePage.vue
+++ .vitepress/theme/components/HomePage.vue
@@ -1,26 +1,26 @@
 <script setup lang="ts">
 import { VPTeamMembers } from 'vitepress/theme'
-import { creators } from '../../creators'
-import { siteName } from '../../../metadata'
+import { creators, siteName } from '../../../metadata'
 </script>
 
 <template>
-  <div class="vp-doc" mt-10 flex flex-col items-center>
-    <h2 id="meet-the-team" font-normal op50 p="t-10 b-2">
-      {{ siteName }} 的创作者
-    </h2>
-    <div w-full p-10>
-      <VPTeamMembers size="small" :members="creators" flex justify-center />
+  <div class="content">
+    <div class="content-container">
+      <main class="main">
+        <div class="vp-doc" mt-10 flex flex-col items-center>
+          <h2 id="meet-the-team" font-normal op50 p="t-10 b-2">
+            {{ siteName }} 的创作者
+          </h2>
+          <div w-full p-10>
+            <VPTeamMembers size="small" :members="creators" flex justify-center />
+          </div>
+        </div>
+      </main>
     </div>
   </div>
 </template>
 
 <style scoped>
-/* 保持简洁的样式，让VitePress默认样式生效 */
-.vp-doc {
-  text-align: center;
-}
-
 h2 {
   background: linear-gradient(135deg, 
     rgb(55, 76, 119) 0%, 
